2e8275b46a8f86b1f304e8ddde5d1113214e60eb
juucustodio <juliano.custodio@hotmail.com.br>
2017-04-05 18:08:01 -0300

    Initial Commit

---
+++ SplashScreen/SplashScreen.Droid/SplashActivity.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SplashScreen.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash",
+              MainLauncher = true,
+              NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Cria sua aplicação aqui
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
